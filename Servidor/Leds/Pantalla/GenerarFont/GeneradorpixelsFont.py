836c4b250c90cf104b96e346d1100f533bd77a68
3131204951M\Xevi <xbrun000@hotmail.com>
2021-03-13 01:41:27 +0100

    Millorar la gestió de la memòria de les fonts

--- Servidor/Leds/Pantalla/GenerarFont/GeneradorpixelsFont.py
+++ Servidor/Leds/Pantalla/GenerarFont/GeneradorpixelsFont.py
@@ -13,9 +13,7 @@ from PIL import Image, ImageFont, ImageDraw
 
 lletres_grans={36,40,41,64,81,91,93,106,123,124,125,175,182,192,193,194,195,196,197,198,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221}
 
-print ('#Declare an array for Arduino.')
-print('#ASCII code / width / heigth')
-print ('int ABECEDARI[][16] = {')
+cadenes =[None,None,None]
 
 for lletres in range(32,127):
 #for lletra in ShowText:
@@ -31,8 +29,8 @@ for lletres in range(32,127):
     image = Image.new('1', size, 1)  #create a b/w image
     draw = ImageDraw.Draw(image)
     draw.text((0, 0), lletra, font=font) #render the text to the bitmap
-    print( '#' + lletra)
-    cadena = '{' + str(lletres) + ',' + str(size[0]) + ','+ str(10) + ','
+    #print( '//' + lletra)
+    cadena = '{' + str(lletres) + ',' #+ str(size[0]) + ',' #+ str(10) + ','
     #for rownum in range(0,size[1]): 
     for colnum in range(size[0]):
         line = ''
@@ -42,15 +40,33 @@ for lletres in range(32,127):
             else: line = line + '1' 
         cadena= cadena + '0b' + line + ","
     #a l'array tot ha de tenir la mateixa mida
-    for extre in range(size[0],13):
-        cadena =  cadena + '0b0,'
+    #for extre in range(size[0],13):
+    #    cadena =  cadena + '0b0,'
     
-    if lletres < 126:
-        print(cadena[0:len(cadena)-1] + '},')
-    else: print(cadena[0:len(cadena)-1] + '}')
+    #if lletres < 126:
+    #    print(cadena[0:len(cadena)-1] + '},')
+    #else     print(cadena[0:len(cadena)-1] + '}')
+    cadenes.append([str(size[0]),lletra,cadena[0:len(cadena)-1] + '}'])
+    
+    
+    
+print('//Declare an array for Arduino.')
+print('//ASCII code / Column 1 / Column 2 / ...')
+#print ('int ABECEDARI[][16] = {')
 
+for mida in range (0,20):
+    primera=0
+    for cadena in cadenes:
+        if cadena is not None:
+            if mida == int(cadena[0]): 
+                if primera == 0 : 
+                    print ('int ABECEDARI' + str(mida) + '[]['+str(mida+1)+'] = {')
+                    primera = 1
+                else: print(',')
+                print("//"+cadena[1])
+                print(cadena[2], end ="")
         
-print('}')
+    if primera != 0: print('};')
     
 ################################    
 #   CODI PER L'ARDUINO 

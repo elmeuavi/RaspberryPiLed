fd448069a676c47623d0068edcecb27be98be5f3
elmeuavi <xbrun000@hotmail.com>
2021-12-12 15:57:13 +0000

    Afegir fitxer de instalació SO

---
+++ instalarSO.sh
@@ -0,0 +1,8 @@
+
+sudo apt-get update
+sudo apt-get -y install python3-pip
+sudo pip3 install adafruit-circuitpython-neopixel
+sudo pip3 install rpi_ws281x
+sudo apt-get -y install build-essential python-dev git scons swig
+sudo python3 -m pip install adafruit-blinka
+

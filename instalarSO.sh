a3e42312466971ec8a22b556a498507d15318a7c
elmeuavi <xbrun000@hotmail.com>
2021-12-12 23:08:30 +0000

    Incorporar html apache

--- instalarSO.sh
+++ instalarSO.sh
@@ -5,4 +5,11 @@ sudo pip3 install adafruit-circuitpython-neopixel
 sudo pip3 install rpi_ws281x
 sudo apt-get -y install build-essential python-dev git scons swig
 sudo python3 -m pip install adafruit-blinka
+sudo apt-get install vsftpd
+sudo apt install apache2
+sudo apt install php libapache2-mod-php php-mysql
+sudo apt-get install -y php-sockets
+echo PENDENT Has de revisar configuració del servidor de FTP a sudo nano /etc/vsftpd.conf
+echo PENDENT Has de revisar activar bus I2C a sudo raspi-config
+
 

3de05e543198424c3316d0a2d0450e4ef2d3e9c7
finger <finger@catalunya.cat>
2021-12-26 18:14:17 +0100

    Controlar la múscica via web

--- Remot/Inicialitzar servidor.bat
+++ Remot/Inicialitzar servidor.bat
@@ -4,25 +4,51 @@ rem set RASPBERRY=192.168.1.144
 rem set RASPBERRY=192.168.43.240
 
 
-FOR /f "tokens=* USEBACKQ" %%G IN (`powershell.exe "(ConvertFrom-StringData(Get-Content '.\configuracio.properties' -raw)).'raspberry.ip'"`) DO (
- set RASPBERRY=%%G
- goto CONTINUAR
-)
-:CONTINUAR
 
+call:LlegirPropietat raspberry.ip RASPBERRY 
+call:LlegirPropietat musica.actiu MUSICA
+call:LlegirPropietat tunnelSSH.actiu TUNELSSH
+
+echo RASPBERRY %RASPBERRY%
+echo MUSICA %MUSICA%
 
 
 echo Incialitzem servidor de la raspberry %RASPBERRY%
 start plink.exe  -t -pw raspberry  -no-antispoof -ssh pi@%RASPBERRY% "sudo python3 /home/pi/RaspberryPiLed/Servidor/Leds/SERVIDOR.py" 
-rem start plink.exe  -t -pw raspberry  -no-antispoof -ssh pi@%RASPBERRY% "sh /home/pi/RaspberryPiLed/Servidor/Leds/setupTunel.bat" 
+
+
+if TUNELSSH EQU 1 (
+	echo Inicialitzem el tunnel SSH
+	start plink.exe  -t -pw raspberry  -no-antispoof -ssh pi@%RASPBERRY% "sh /home/pi/RaspberryPiLed/Servidor/Leds/setupTunel.bat" 
+)else (
+	echo NO inicialitzarem el tunnel SSH
+)
+
 
 if exist %userprofile%\Documents\RaspberryPiLed\Remot\ControladoraLedTCPIP.py (
 	cd %userprofile%\Documents\RaspberryPiLed\Remot\
-	
-	echo incialitzem servidor de Musica per Halloween
-	start python MusicaPCLocal.py
+
+	if %MUSICA% EQU 1 (
+		echo incialitzem servidor de Musica per Halloween
+		start python MusicaPCLocal.py
+	)else (
+		echo NO inicialitzem la música
+	)
 	
 	echo incialitzem controladora
 	python ControladoraLedTCPIP.py
 )
 
+goto:eof
+
+
+
+
+:LlegirPropietat
+
+FOR /f "tokens=* USEBACKQ" %%G IN (`powershell.exe "(ConvertFrom-StringData(Get-Content '.\configuracio.properties' -raw)).'%~1'"`) DO (
+ set %~2=%%G
+ goto CONTINUAR
+)
+:CONTINUAR
+goto:eof
--- Remot/MusicaPCLocal.py
+++ Remot/MusicaPCLocal.py
@@ -2,11 +2,12 @@
 import socket
 import sys
 import select
+import traceback
 
 
 #python -m pip install pygame
 import pygame # per la musica
-
+import time
 
 
 if __name__ == '__main__':
@@ -16,35 +17,44 @@ if __name__ == '__main__':
     print ('comandos: musica, close, stop');
     print ('');
     
+    pygame.mixer.init(frequency=48000)
     
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = ('', 20000)
     sock.bind(server_address)
     print  ('starting up on ' , sock.getsockname())
-    sock.listen(1)
+    sock.listen(5)
   
-    
+    inputs=[sock]
 
+    try:
     
-    while True:
-        print  ('Waiting for a new connection')
-        connection, client_address = sock.accept()
-        try:
-            print  ('client connected:', client_address)
-            while True:
+            while inputs:
+#        print  ('Waiting for a new connection')
+#        connection, client_address = sock.accept()
+ #           print  ('client connected:', client_address)
+ #           while True:
                 #https://steelkiwi.com/blog/working-tcp-sockets/
                 #https://bip.weizmann.ac.il/course/python/PyMOTW/PyMOTW/docs/select/index.html
-                read_sockets, writable, exceptional  = select.select([connection] , [], [connection])
+                read_sockets, writable, exceptional  = select.select(inputs , [], inputs,1)
+                
 
-
-                    
+                if pygame.mixer.music.get_busy(): print(".", end = "",flush=True)
+                
                 for s in read_sockets:
+                    if s == sock:
+                        connection, client_address = s.accept()
+                        connection.setblocking(0)
+                        inputs.append(connection)
+                        #message_queues[connection] = Queue.Queue()
+                        print ("Nova connexio arribada: " + str(client_address) + " Connexions Actives: " + str(len(inputs)))
+                    
+                
                     #mirem la conexió per wifi del PC
-                    if s == connection:
-                        print("Rebuda de la wifi")
+                    else:
                         try:
-                            data = connection.recv(65535).decode("utf-8") 
+                            data = s.recv(65535).decode("utf-8") 
                         except:
                             print("Error en la connexió al intentar llegir");
                             break;
@@ -57,12 +67,11 @@ if __name__ == '__main__':
                                     if len(linia_comanda) > 0 :  print  ('Comanda rebuda: %s ' % linia_comanda.replace('\n', ''))
                                     
                                     #Retornar al client la mateixa informació (no ho fem)
-                                    #if debug: connection.sendall(bytearray(linia_comanda, 'utf-8'))
+                                    #if debug: s.sendall(bytearray(linia_comanda, 'utf-8'))
                                     comanda = linia_comanda.split()           
-                                    print ("-" + comanda[0] + "-")
                                     if comanda[0] == "musica": 
                                         print ('dins música')
-                                        if pygame.mixer.music.get_busy:
+                                        if pygame.mixer.music.get_busy():
                                             print('Ja estem reproduint música, no fem res!')
                                         else:
                                             pygame.mixer.init(frequency=48000)
@@ -72,26 +81,33 @@ if __name__ == '__main__':
                                                 pygame.mixer.music.load('data\\MusicaHalloween.mp3')
                                             pygame.mixer.music.set_volume(1)
                                             pygame.mixer.music.play()  # start=POSICIO_MP3/1000
+
                                     elif comanda[0] == "stop": 
                                         pygame.mixer.music.stop()
                                     elif comanda[0] == "close": 
                                         sys.exit()
                                         
+                                    s.close();
+                                    inputs.remove(s)
+                                        
                         else:
                            print("Client desconectat")
                            s.close();
+                           inputs.remove(s)
                           
-                #Mirem si s'ha tancat la connexió
-                if connection in exceptional: 
-                    print("Connexió amb excepció")
-                    break;
+                    #Mirem si s'ha tancat la connexió
+                    if s in exceptional: 
+                        print("Connexió amb excepció")
+                        break;
+                        
+                    
 
 
-        except Exception as e: 
-            print("Tenim una excepció i anem a tancar les connexions")
-            print(e)
-            traceback.print_exc()
-            sock.close()
-        finally:
-            print("Anem a tancar les connexions")
-            sock.close()
+    except Exception as e: 
+        print("Tenim una excepció i anem a tancar les connexions")
+        print(e)
+        traceback.print_exc()
+        sock.close()
+    finally:
+        print("Anem a tancar les connexions")
+        sock.close()

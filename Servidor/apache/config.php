993e5df497b58cb3725ea87b7accc61587bf475a
elmeuavi <xbrun000@hotmail.com>
2021-12-28 01:19:22 +0000

    canviar IP a VPN

--- Servidor/apache/config.php
+++ Servidor/apache/config.php
@@ -3,7 +3,8 @@
     $config['musica']['ip']         	= '192.168.1.189';
 	$config['musica']['port']         	= 20000;
     $config['musica']['actiu']          = 1;
-	$config['leds']['ip']         		= '127.0.0.1';
+	//$config['leds']['ip']         		= '127.0.0.1';
+	$config['leds']['ip']         		= '10.8.0.10';
 	$config['leds']['port']         	= 10000;
 	$config['leds']['TunnelSSH']        = 0;
 ?>    
